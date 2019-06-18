68bfaaf6cc4f477ba1a8ef38b4bb44713424de5a
Alex <Alex@AlexLaptop>
2019-06-18 19:09:48 +1200

    dates semi work

--- Excel-To-Invoice/Excel-To-Invoice/src/com/powerhouseag/excel/Excel.java
+++ Excel-To-Invoice/Excel-To-Invoice/src/com/powerhouseag/excel/Excel.java
@@ -11,7 +11,6 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.threeten.bp.LocalDate;
 
-@SuppressWarnings("deprecation")
 public class Excel {
 	
 	public Excel(String file) {
