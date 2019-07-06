8c9f3812ee6971a5ce39c6609c2e9d28d0b9a520
Alex <Alex@AlexLaptop>
2019-07-06 18:02:07 +1200

    made functions independent of the parent class

--- Excel-To-Invoice/Excel-To-Invoice/src/com/powerhouseag/xero/ProcessServlet.java
+++ Excel-To-Invoice/Excel-To-Invoice/src/com/powerhouseag/xero/ProcessServlet.java
@@ -26,39 +26,31 @@ import com.xero.models.accounting.Invoice.StatusEnum;
 import com.xero.models.accounting.Invoice.TypeEnum;
 import com.xero.models.accounting.Invoices;
 import com.xero.models.accounting.LineItem;
-import com.xero.models.accounting.RepeatingInvoice;
-import com.xero.models.accounting.RepeatingInvoices;
-import com.xero.models.accounting.Schedule;
-import com.xero.models.accounting.Schedule.UnitEnum;
 
 public class ProcessServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	// private final String UPLOAD_DIRECTORY = "./WEB-INF/files";
 	private final String UPLOAD_DIRECTORY = "c:/temp/excel2invoiceuploads";
-	private int noFiles;
-	private String[] fileNames;
 	private String[] failedProcessing = new String[100];
-	private File fileObject;
-	private Invoices serverInvoices;
 	private AccountingApi accountingApi;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
 		// recieve uploaded files
-		recieve(request);
+		File[] files = recieve(request);
 
 		// api to access xero database
 		accountingApi = ApiStorage.getAccountingApi();
 
 		// take excel data and put onto invoice
-		parseSingleExcel();
-		
+		Invoices serverInvoices = parseSingleExcel(files);
+
 		// take excel data and put onto invoice
 		//parseRepeatingExcel();
 
 		// attach pdfs to invoices on server
-		attachPdfs();
+		attachPdfs(serverInvoices, files);
 
 		// print to screen that transfer has succeeded
 		printSuccess(response);
@@ -66,33 +58,36 @@ public class ProcessServlet extends HttpServlet {
 		// delete all files in upload directory
 		deleteFiles();
 	}
-	
-	
+
+
 
 	//#######################################################
 	//function to receive file from upload servlet
-	private void recieve(HttpServletRequest request) {
-		// Receive file
+	private File[] recieve(HttpServletRequest request) {
+
+		// array for files that were uploaded
+		File[] files = null;
+
+		// Receive files
 		try {
 			List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
-			noFiles = multiparts.size()-1;
-			fileNames = new String[noFiles];
+			int noFiles = multiparts.size()-1;
+			files = new File[noFiles];
 			for(int i = 0;i < multiparts.size(); i++) {
 				FileItem item = multiparts.get(i);
 				if(!item.isFormField()){
 					String name = new File(item.getName()).getName();
-					fileNames[i] = name;
-					System.out.println(fileNames[i]);
-					item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
+					item.write(files[i] = new File(UPLOAD_DIRECTORY + File.separator + name));
 				}
 			}
 			//File uploaded successfully
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
+		return files;
 	}
 
-	
+
 	//############################################################
 	// function to print to the screen that transfer has succeeded
 	private void printSuccess(HttpServletResponse response) {
@@ -135,21 +130,21 @@ public class ProcessServlet extends HttpServlet {
 			e1.printStackTrace();
 		}
 	}
-	
+
 	//#######################################
 	// function to attach pdfs to invoices
-	private void attachPdfs() throws IOException {
+	private void attachPdfs(Invoices serverInvoices, File[] files) throws IOException {
 		for(int i = 0; i < serverInvoices.getInvoices().size(); i++) {
 
-			File pdf = new File(UPLOAD_DIRECTORY+File.separator+fileNames[i]+".pdf");
+			File pdf = new File(files[i].getAbsolutePath()+".pdf");
 			try {
-				ExcelToPdf.createPdf(pdf, new File(UPLOAD_DIRECTORY+File.separator+fileNames[i]));
+				ExcelToPdf.createPdf(pdf, files[i]);
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
 			InputStream is = new FileInputStream(pdf);
 			byte[] bytes = IOUtils.toByteArray(is);
-			
+
 			System.out.println(serverInvoices.getInvoices().get(i).getInvoiceID());
 
 			try {
@@ -160,7 +155,7 @@ public class ProcessServlet extends HttpServlet {
 
 		}
 	}
-	
+
 	//#####################################################
 	// function to delete all files in the upload directory
 	private void deleteFiles() {
@@ -171,14 +166,153 @@ public class ProcessServlet extends HttpServlet {
 			System.out.println("Deleting file : "+file.getName());
 		}
 	}
-	
+
 	//########################################################################
 	// function to take data from single excel sheet and put into xero invoice object
-	private void parseSingleExcel() {
+	private Invoices parseSingleExcel(File[] uploadFiles) {
 
 		// invoices to get uploaded to xero
 		Invoices invoices = new Invoices();
 		
+		// invoices from xero's servers
+		Invoices serverInvoices = new Invoices();
+
+		// files that were uploaded
+		File[] files = uploadFiles;
+
+		for(int i = 0; i < uploadFiles.length; i++) {
+
+			// putting files into excel objects
+			Excel excel = new Excel(files[i].getAbsolutePath());
+
+			// date due and date made
+			LocalDate dueDate;
+			LocalDate madeDate;
+
+			LocalDate date = LocalDate.now();
+
+			String name = "Someone";
+			double subTotal = 0.00;
+			String invoiceNumber = "An Invoice Number";
+
+			// using try/catch to see if any files fail and keep processing others
+			try {
+				// get name from excel sheet
+				name = excel.getName();
+			}catch(NullPointerException e) {
+				failedProcessing[i] = files[i].getName();
+			}
+
+			try {
+				// get invoice number from excel sheet
+				invoiceNumber = excel.getInvoiceNo();
+			}catch(NullPointerException e) {
+				failedProcessing[i] = files[i].getName();
+			}
+
+			try {
+				// get date from excel sheet
+				date = excel.getInvoiceDate();
+			}catch(NullPointerException e) {
+				failedProcessing[i] = files[i].getName();
+			}
+
+			try {
+				// get sub total from excel sheet
+				subTotal = excel.getSubTotal()/4;
+			}catch(NullPointerException e) {
+				failedProcessing[i] = files[i].getName();
+			}
+
+			// extracting individual year, month, and day integers and adjusting to actual values
+			//int year = date.getYear()-70;
+			int year = 2019;
+			//int month = date.getMonthValue()+1;
+			int month = 6;
+			//int day = date.getDayOfMonth()-1;
+			int day = 20;
+
+			// date object for date invoice was made
+			//madeDate = LocalDate.of(year, month-1, day);
+			madeDate = LocalDate.of(year, month-1, day);
+
+			// if month is bigger than 12 reset back to one and add year
+			if(month>=13) {
+				month-=12;
+				year+=1;
+			}
+
+			// due date object gets set to 20th of next month
+			dueDate = LocalDate.of(year, month, 20);
+			
+			// line on invoice
+			LineItem lineItem = new LineItem();
+
+			lineItem.setLineAmount(subTotal);
+			lineItem.setUnitAmount(subTotal);
+
+			// invoice object
+			Invoice invoice = new Invoice();
+
+			//configuration for end of year final invoices
+			//lineItem.setDescription("2019 Final");
+			//lineItem.setQuantity(1.0);
+			//lineItem.setTaxType("OUTPUT2");
+			//lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
+
+			//configuration for start of year annual invoices
+			//lineItem.setDescription("May-May Grazing");
+			//lineItem.setQuantity(1.0);
+			//lineItem.setTaxType("OUTPUT2");
+			//lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
+
+			//configuration for start of year cow grazing invoices
+			lineItem.setDescription("2019 Winter Cow Grazing");
+			lineItem.setQuantity(1.0);
+			lineItem.setTaxType("OUTPUT2");
+			lineItem.setAccountCode("1400"); // "1400 - Grazing Income"
+
+			// contact to send invoice to
+			Contact contact = new Contact();
+			contact.setName(name);
+
+			// if invoice total is negative, invert total and change to accounts payable
+			if(lineItem.getUnitAmount() >= 0.0) {
+				invoice.setType(TypeEnum.ACCREC);
+			}else {
+				invoice.setType(TypeEnum.ACCPAY);
+				lineItem.setUnitAmount(-lineItem.getUnitAmount());
+				lineItem.setLineAmount(-lineItem.getLineAmount());
+			}
+
+			invoice.addLineItemsItem(lineItem);
+			invoice.setContact(contact);
+			invoice.setDueDate(dueDate);
+			invoice.setDate(madeDate);
+			invoice.setReference(invoiceNumber);
+			invoice.setStatus(StatusEnum.SUBMITTED);
+
+			// adding invoice items to the object that gets sent to the xero servers
+			invoices.addInvoicesItem(invoice);
+		}
+
+		// add invoices to xero database
+		try {
+			serverInvoices = accountingApi.createInvoice(invoices, true);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return serverInvoices;
+	}
+
+	//########################################################################
+	// function to take data from repeating excel sheet and put into xero invoice object
+	/*private void parseRepeatingExcel() {
+
+		// invoices to get uploaded to xero
+		RepeatingInvoices invoices = new RepeatingInvoices();
+
 		for(int i = 0; i < noFiles; i++) {
 
 			// files that were uploaded
@@ -246,23 +380,20 @@ public class ProcessServlet extends HttpServlet {
 
 			// line on invoice
 			LineItem lineItem = new LineItem();
-			
-				lineItem.setLineAmount(subTotal);
-				lineItem.setUnitAmount(subTotal);
 
-				// due date object gets set to 20th of next month
-				dueDate = LocalDate.of(year, month, 20);
+			RepeatingInvoice invoice = new RepeatingInvoice();
+			lineItem.setLineAmount(subTotal/12);
+			lineItem.setUnitAmount(subTotal/12);
 
-				// invoice object
-				Invoice invoice = new Invoice();
-			
+			// due date object gets set to 20th of next month
+			dueDate = LocalDate.of(year, month, 20);
 
 			//configuration for end of year final invoices
-//			lineItem.setDescription("2019 Final");
-//			lineItem.setQuantity(1.0);
-//			lineItem.setTaxType("OUTPUT2");
-//			lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
-			
+			//				lineItem.setDescription("2019 Final");
+			//				lineItem.setQuantity(1.0);
+			//				lineItem.setTaxType("OUTPUT2");
+			//				lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
+
 			//configuration for start of year annual invoices
 			//lineItem.setDescription("May-May Grazing");
 			//lineItem.setQuantity(1.0);
@@ -281,171 +412,39 @@ public class ProcessServlet extends HttpServlet {
 
 			// if invoice total is negative, invert total and change to accounts payable
 			if(lineItem.getUnitAmount() >= 0.0) {
-				invoice.setType(TypeEnum.ACCREC);
+				invoice.setType(RepeatingInvoice.TypeEnum.ACCREC);
 			}else {
-				invoice.setType(TypeEnum.ACCPAY);
+				invoice.setType(RepeatingInvoice.TypeEnum.ACCPAY);
 				lineItem.setUnitAmount(-lineItem.getUnitAmount());
 				lineItem.setLineAmount(-lineItem.getLineAmount());
 			}
 
+			// schedule for repeating invoices
+			Schedule schedule = new Schedule();
+			schedule.setUnit(UnitEnum.MONTHLY);
+			schedule.setStartDate(madeDate);
+			schedule.setEndDate(dueDate);
+
+			// set invoice parameters
 			invoice.addLineItemsItem(lineItem);
 			invoice.setContact(contact);
-			invoice.setDueDate(dueDate);
-			invoice.setDate(madeDate);
 			invoice.setReference(invoiceNumber);
-			invoice.setStatus(StatusEnum.SUBMITTED);
+			invoice.setStatus(RepeatingInvoice.StatusEnum.AUTHORISED);
+			invoice.setSchedule(schedule);
 
-			// adding invoice items to the object that gets sent to the xero servers
-			invoices.addInvoicesItem(invoice);
+			// adding invoice items to object that gets sent to the xero servers
+			invoices.addRepeatingInvoicesItem(invoice);
 		}
-		
+
 		// add invoices to xero database
-		try {
-			serverInvoices = accountingApi.createInvoice(invoices, true);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//########################################################################
-		// function to take data from repeating excel sheet and put into xero invoice object
-		private void parseRepeatingExcel() {
-
-			// invoices to get uploaded to xero
-			RepeatingInvoices invoices = new RepeatingInvoices();
-			
-			for(int i = 0; i < noFiles; i++) {
-
-				// files that were uploaded
-				fileObject = new File(UPLOAD_DIRECTORY+File.separator+fileNames[i]);
-
-				// putting files into excel objects
-				Excel excel = new Excel(fileObject.getAbsolutePath());
-
-				// date due and date made
-				LocalDate dueDate;
-				LocalDate madeDate;
-
-				LocalDate date = LocalDate.now();
-
-				String name = "Someone";
-				double subTotal = 0.00;
-				String invoiceNumber = "An Invoice Number";
-
-				// using try/catch to see if any files fail and keep processing others
-				try {
-					// get name from excel sheet
-					name = excel.getName();
-				}catch(NullPointerException e) {
-					failedProcessing[i] = fileNames[i];
-				}
-
-				try {
-					// get invoice number from excel sheet
-					invoiceNumber = excel.getInvoiceNo();
-				}catch(NullPointerException e) {
-					failedProcessing[i] = fileNames[i];
-				}
-
-				try {
-					// get date from excel sheet
-					date = excel.getInvoiceDate();
-				}catch(NullPointerException e) {
-					failedProcessing[i] = fileNames[i];
-				}
-
-				try {
-					// get sub total from excel sheet
-					subTotal = excel.getSubTotal()/4;
-				}catch(NullPointerException e) {
-					failedProcessing[i] = fileNames[i];
-				}
-
-				// extracting individual year, month, and day integers and adjusting to actual values
-				//int year = date.getYear()-70;
-				int year = 2019;
-				//int month = date.getMonthValue()+1;
-				int month = 6;
-				//int day = date.getDayOfMonth()-1;
-				int day = 20;
-
-				// date object for date invoice was made
-				//madeDate = LocalDate.of(year, month-1, day);
-				madeDate = LocalDate.of(year, month-1, day);
-
-				// if month is bigger than 12 reset back to one and add year
-				if(month>=13) {
-					month-=12;
-					year+=1;
-				}
-
-				// line on invoice
-				LineItem lineItem = new LineItem();
-				
-				RepeatingInvoice invoice = new RepeatingInvoice();
-					lineItem.setLineAmount(subTotal/12);
-					lineItem.setUnitAmount(subTotal/12);
-
-					// due date object gets set to 20th of next month
-					dueDate = LocalDate.of(year, month, 20);
-					
-				//configuration for end of year final invoices
-//				lineItem.setDescription("2019 Final");
-//				lineItem.setQuantity(1.0);
-//				lineItem.setTaxType("OUTPUT2");
-//				lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
-				
-				//configuration for start of year annual invoices
-				//lineItem.setDescription("May-May Grazing");
-				//lineItem.setQuantity(1.0);
-				//lineItem.setTaxType("OUTPUT2");
-				//lineItem.setAccountCode("1502"); // "1502 - Grazing - May-May Heifers"
-
-				//configuration for start of year cow grazing invoices
-				lineItem.setDescription("2019 Winter Cow Grazing");
-				lineItem.setQuantity(1.0);
-				lineItem.setTaxType("OUTPUT2");
-				lineItem.setAccountCode("1400"); // "1400 - Grazing Income"
-
-				// contact to send invoice to
-				Contact contact = new Contact();
-				contact.setName(name);
-
-				// if invoice total is negative, invert total and change to accounts payable
-				if(lineItem.getUnitAmount() >= 0.0) {
-					invoice.setType(RepeatingInvoice.TypeEnum.ACCREC);
-				}else {
-					invoice.setType(RepeatingInvoice.TypeEnum.ACCPAY);
-					lineItem.setUnitAmount(-lineItem.getUnitAmount());
-					lineItem.setLineAmount(-lineItem.getLineAmount());
-				}
-
-				// schedule for repeating invoices
-				Schedule schedule = new Schedule();
-				schedule.setUnit(UnitEnum.MONTHLY);
-				schedule.setStartDate(madeDate);
-				schedule.setEndDate(dueDate);
-				
-				// set invoice parameters
-				invoice.addLineItemsItem(lineItem);
-				invoice.setContact(contact);
-				invoice.setReference(invoiceNumber);
-				invoice.setStatus(RepeatingInvoice.StatusEnum.AUTHORISED);
-				invoice.setSchedule(schedule);
-
-				// adding invoice items to object that gets sent to the xero servers
-				invoices.addRepeatingInvoicesItem(invoice);
-			}
-			
-			// add invoices to xero database
-			/*
+		/*
 			try {
 				serverInvoices = accountingApi.createInvoice(invoices, true);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			*/
-		}
+		 //
+	}*/
 
 
 }
